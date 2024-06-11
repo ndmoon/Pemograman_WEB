6144b3da5254b0aee403f4b9a4daedfd253f7e1b
nadiamonika <115436702+ndmoon@users.noreply.github.com>
2024-06-11 12:37:45 +0700

    PHP & HTML

---
+++ Tugas/simulasi UTS/javascriptLatihan.js
@@ -0,0 +1,6 @@
+function hitungluas(a,t){
+    var luas = (a*t)/2
+    document.getElementById('alas').innerHTML = a
+    document.getElementById('tinggi').innerHTML = t
+    document.getElementById('luas').innerHTML = luas
+}
